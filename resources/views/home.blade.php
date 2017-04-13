efc1564f914b7f5cedf049f65d86a5cc5d3bf608
Attila Katona <katona.skmobil@gmail.com>
2017-04-13 20:04:03 +0000

    set post and get routing without validation

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+@section('content')
+    <div class='centered'>
+     <a href="{{ route('niceaction',['action' => 'greet']) }}">Greet</a>
+     <a href="{{ route('niceaction',['action' => 'hug']) }}">Hug</a>
+     <a href="{{ route('niceaction',['action' => 'kiss']) }}">Kiss</a>
+     <br>
+     <form action="{{ route('benice') }}" method="post">
+         <label for="select-action">I want to..</label>
+         <select name="action" id="select-action">
+             <option value="greet">Greet</option>
+             <option value="hug">Hug</option>
+             <option value="kiss">Kiss</option>
+         </select>
+         <input type="text" name="name"/>
+         <button type="submit">Action</button>
+         <input type="hidden" value="{{ Session::token() }}"name="_token">
+     </form>
+    </div>
+@endsection
